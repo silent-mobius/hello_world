6ab9b0f9a99fe95e96466f9b5ce24ff675263721
silent-mobius <alex@vaiolabs.com>
2020-03-12 13:48:31 +0200

    adding c

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+############################################################
+#created by : Pushtakio
+#purpose: to say hello to world
+#version: 1.0.0
+#date: 01/03/2020
+############################################################
+*/
+
+int main(){
+char arr[]="Hello World";
+
+printf("%s",&arr);
+
+return 0;
+}
