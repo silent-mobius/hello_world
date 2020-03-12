ce6f1a9d26638cdae1fa85ed41062e66a2bb5b5e
silent-mobius <alex@vaiolabs.com>
2020-03-12 13:47:38 +0200

    adding python scirpt

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+############################################################
+#created by : Pushtakio
+#purpose: to say hello to world
+#version: 1.0.0
+#date: 01/03/2020
+############################################################
+
+
+print("Hello World")
