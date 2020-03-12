2786f24561836a6830248cbde1bf2127359ad56c
silent-mobius <alex@vaiolabs.com>
2020-03-12 13:47:29 +0200

    adding shell script

---
+++ hello_world.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+############################################################
+#created by : Pushtakio
+#purpose: to say hello to world
+#version: 1.0.0
+#date: 01/03/2020
+############################################################
+
+
+#echo "Hello World"
+
+printf "\n\t%s\n" "Hello World"
